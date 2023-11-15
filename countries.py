ea2ac3e4e947d2bb0c5a0b35000253f231d1f4f4
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-11-15 17:18:50 +0700

    Add files via upload

---
+++ countries.py
@@ -0,0 +1,34 @@
+input_filename = "country_info.txt"
+
+countries = {}
+with open(input_filename) as country_file:
+    country_file.readline()
+    for row in country_file:
+        data = row.strip("\n").split("|")
+        country, capital, code, code3, dialing, timezone, currency = data
+        country_dict = {
+            'name': country,
+            'capital': capital,
+            'country_code': code,
+            'cc3': code3,
+            'dialing_code': dialing,
+            'timezone': timezone,
+            'currency': currency,
+        }
+
+        print(country_dict)
+        countries[country.casefold()] = country_dict
+        countries[code.casefold()] = country_dict
+
+while True:
+    chosen_country = input("Please enter the name or code of a country (type 'quit' to exit): ")
+    country_key = chosen_country.casefold()
+
+    if country_key == "quit":
+        break
+
+    if country_key in countries:
+        country_data = countries[country_key]
+        print(f"The capital of {chosen_country} is {country_data['capital']}")
+    else:
+        print(f"Country or code '{chosen_country}' not found. Please try again.")
